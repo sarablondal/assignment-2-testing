30e0567c0308c26c8b4ab06dd9c068ef80c714b9
sarablondal <sarablondal@users.noreply.github.com>
2022-01-31 18:11:43 +0000

    tests completed and datetypes added

--- src/__tests__/dateUtils.test.ts
+++ src/__tests__/dateUtils.test.ts
@@ -1,16 +1,76 @@
+import { DATE_UNIT_TYPES } from "../constants";
 import { getCurrentYear, add, isWithinRange, isDateBefore, isSameDay} from "../dateUtils"
-describe("Date Utils", () => {
-    it('should return new date', () => {
-      const date = new Date();
-    }),
-    it('should return new year', () => {
+
+const date_type = DATE_UNIT_TYPES;
+
+describe("Get current year", () => {
+    it('should return current year', () => {
       const result = getCurrentYear();
       expect(result).toBe(2022);
      })
-    // it('should add a new date'), () => {
-    //   const date = add(new Date(), 1);
-    // },
-    // it('should check if date is in range', () => {
-    //   const result = new Date();
-  
 });
+
+describe("Add a new date unit to a date", () => {
+  it("Should add x many days to date", () => {
+    const date = new Date(2022, 0, 1);
+    const new_date = add(date, 1);
+    expect(new_date.getDate()).toBe(2);
+  });
+  it("Should add x many months to date", () => {
+    const date = new Date(2022, 0, 1);
+    const new_date = add(date, 1, date_type.MONTHS);
+    expect(new_date.getDate()).toBe(1);
+  });
+  it("Should add x many years to date", () => {
+    const date = new Date(2022, 0, 1);
+    const new_date = add(date, 1, date_type.YEARS);
+    expect(new_date.getDate()).toBe(1);
+  });
+});
+
+describe('Check whether date is within range', () => {
+  it('should return true if date is within range', () => {
+    const date = new Date(2022, 2, 2);
+    const date_from = new Date(2021, 0, 1);
+    const date_to = new Date(2022, 3, 3);
+    const date_range = isWithinRange(date, date_from, date_to);
+    expect(date_range).toBe(true);
+  });
+  it('should return false if date is outside range', () => {
+    const date = new Date(2022, 2, 2);
+    const date_from = new Date(2021, 0, 1);
+    const date_to = new Date(2022, 1, 1);
+    const date_range = isWithinRange(date, date_from, date_to);
+    expect(date_range).toBe(false);
+  });
+});
+
+describe('Check whether date is before date', () => {
+  it('should return true if date is before compared date', () => {
+    const date = new Date(2022, 0, 1);
+    const compare_date = new Date(2022, 1, 1);
+    const date_is_before = isDateBefore(date, compare_date);
+    expect(date_is_before).toBe(true);
+  });
+  it('should return false if date is not before compared date', () => {
+    const date = new Date(2022, 2, 1);
+    const compare_date = new Date(2022, 1, 1);
+    const date_is_before = isDateBefore(date, compare_date);
+    expect(date_is_before).toBe(false);
+  });
+});
+
+describe('Check whether date is the same day', () => {
+  it('should return true if date is the same day', () => {
+    const date = new Date(2022, 1, 1);
+    const other_date = new Date(2022, 1, 1);
+    const is_same_date = isSameDay(date, other_date);
+    expect(is_same_date).toBe(true);
+  });
+  it('should return false if date is not the same day', () => {
+    const date = new Date(2022, 1, 1);
+    const other_date = new Date(2022, 2, 3);
+    const is_same_date = isSameDay(date, other_date);
+    expect(is_same_date).toBe(false);
+  })
+})
