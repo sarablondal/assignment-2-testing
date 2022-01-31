8d58d937628cf9a2b0c2371cfa1099d4df6d2a2a
sarablondal <sarablondal@users.noreply.github.com>
2022-01-31 17:34:06 +0000

    first commit

--- src/__tests__/dateUtils.test.ts
+++ src/__tests__/dateUtils.test.ts
@@ -1,3 +1,16 @@
+import { getCurrentYear, add, isWithinRange, isDateBefore, isSameDay} from "../dateUtils"
 describe("Date Utils", () => {
-  // Add your tests here
+    it('should return new date', () => {
+      const date = new Date();
+    }),
+    it('should return new year', () => {
+      const result = getCurrentYear();
+      expect(result).toBe(2022);
+     })
+    // it('should add a new date'), () => {
+    //   const date = add(new Date(), 1);
+    // },
+    // it('should check if date is in range', () => {
+    //   const result = new Date();
+  
 });
